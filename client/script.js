555cd49c9c2bb389ae6ddadd8a441ccf407bff09
Hassan Alti <121572597+HassanAlti@users.noreply.github.com>
2023-07-07 21:42:59 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("https://hassbot-gh0l.onrender.com/", {
+  const response = await fetch("https://chatwidget.onrender.com/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
