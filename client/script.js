670c5fa6dd59592fb856cbec3b482e71dddc8c38
Hassan Alti <121572597+HassanAlti@users.noreply.github.com>
2023-10-27 23:50:30 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -116,7 +116,7 @@ const originalPrompt = data.get("prompt");
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "name" : "gf"
+      "name" : "motivation"
     },
     body: JSON.stringify({
       prompt:  originalPrompt,
